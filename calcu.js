b17804a0cc710bccf016126c687fda3e854c5994
dollybiwal <dbiwal28@gmail.com>
2024-12-19 13:22:27 +0530

    Add files via upload

--- calcu.js
+++ calcu.js
@@ -1,54 +1,16 @@
 var x, y, result
 
-function input1() {
-   document.getElementById("input").value+="1"
+function appendtodisplay(value) {
+   document.getElementById("display").value+= value;
 }
-function input2() {
+function cleardisplay() {
 
-   document.getElementById("input").value+="2"
+   document.getElementById("display").value='';
 }
-function input3() {
-
-   document.getElementById("input").value+="3"
+function calculate() {
+      try {
+   document.getElementById("display").value= eval(document.getElementById("display").value);
+} catch(e)
+ { document.getElementById("display").value= 'Error'   
 }
-function input4() {
-
-   document.getElementById("input").value+="4"
-}
-function input5() {
-
-   document.getElementById("input").value+="5"
-}
-function input6() {
-
-   document.getElementById("input").value+= "6"
-}
-function input7() {
-
-   document.getElementById("input").value+= "7"
-}
-function input8() {
-
-   document.getElementById("input").value+= "8"
-}
-function input9() {
-
-   document.getElementById("input").value+= "9"
-}
-function input0() {
-
-   document.getElementById("input").value+= "0"
-}
-function inputplus() {
-   document.getElementById("inputplus").value+= "+"
-}
-function inputsub() {
-   document.getElementById("input").value+= "-"
-}
-function inputmulti() {
-   document.getElementById("input").value+= "*"
-}
-
-function calculate(){
-   document.getElementById("inputbox").value=eval(document.getelementbyid("inputbox").value)
 }
